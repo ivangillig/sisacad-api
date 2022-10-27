5ba51b7ae8e65a4d364588e77f67f0ea2c6f182b
Ivan Gillig <ivan.gillig@gmail.com>
2022-10-27 17:28:11 -0300

    models added

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -15,8 +15,12 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -106,16 +110,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# DATABASES = { 
-#     'default': { 
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'), 
-#     } 
-# }
 
 # DATABASES = {
 #         'default': {
