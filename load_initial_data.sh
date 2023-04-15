485274ba2f49d8056d51e8b4075bf18690ebf257
ivan gillig <ivan.gillig@gmail.com>
2023-04-15 16:47:45 -0300

    Se agregan datos de prueba

--- load_initial_data.sh
+++ load_initial_data.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#Usuarios
 python3 manage.py loaddata apps/users/fixtures/users_data.json; \
+python3 manage.py loaddata apps/users/fixtures/persons_data.json;
+
+
+#Administración
 python3 manage.py loaddata apps/administracion/fixtures/division_data.json;
 python3 manage.py loaddata apps/administracion/fixtures/subjects_data.json;
 python3 manage.py loaddata apps/administracion/fixtures/specialities_data.json;
 python3 manage.py loaddata apps/administracion/fixtures/levels_data.json;
 python3 manage.py loaddata apps/administracion/fixtures/grades_data.json;
+
+#Alumnos
+python3 manage.py loaddata apps/alumnos/fixtures/students_data.json;
