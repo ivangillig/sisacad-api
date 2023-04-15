a7bb359a33abb626340156f202dbe6d29e861eea
ivan gillig <ivan.gillig@gmail.com>
2023-04-14 23:25:14 -0300

    Se agregan datos iniciales

--- load_initial_data.sh
+++ load_initial_data.sh
@@ -5,3 +5,4 @@ python3 manage.py loaddata apps/administracion/fixtures/division_data.json;
 python3 manage.py loaddata apps/administracion/fixtures/subjects_data.json;
 python3 manage.py loaddata apps/administracion/fixtures/specialities_data.json;
 python3 manage.py loaddata apps/administracion/fixtures/levels_data.json;
+python3 manage.py loaddata apps/administracion/fixtures/grades_data.json;
